4e53a7f4ffc2c587dc9e7371a766d304b78d64f0
AKASH PRATHAP <71762864+Akashprathap@users.noreply.github.com>
2025-07-15 08:38:09 +0100

    Add files via upload

---
+++ Day 6 - Python Conditions.py
@@ -0,0 +1,26 @@
+#Python Conditions
+
+'''a = 33
+b = 200
+if b > a:
+    print("b is greater than a",b)'''
+
+'''a = 33
+b = 33
+if b > a:
+   print("b is greater than a",b)
+elif a == b:
+    print("a and b are equal")'''
+
+'''a = 200
+b = 33
+if b > a:
+   print("b is greater than a",b)
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")'''
+
+a = int(input("enter a number: "))
+print(a)
+
---
+++ Day 6 - Python loops.py
@@ -0,0 +1,36 @@
+#Python loops
+
+'''i = 1
+while i > 0:
+    print(i)
+    i+= 1'''
+
+'''i = 1
+while i < 6:
+    print(i)
+    i += 1'''
+    
+'''i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1'''
+
+'''i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)'''
+
+'''for x in range(6):
+    print(x)
+else:
+    print("finally finished")'''
+
+adj = ["red","big","tasty"]
+fruits = ["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)'''
